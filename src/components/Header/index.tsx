561b7d9b95106c624c8c15008668127493747b97
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 16:43:28 -0300

    ADding header components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import logoImg from "../../assets/images/logo.svg";
-import { HeaderContainer } from "./styles";
+import { HeaderContainer, HeaderContent } from "./styles";
 
 
 export function Header() {
     return (
         <HeaderContainer>
-            <img src={logoImg} alt="Logo da DT Money"></img>
-            <button type="button">Nova Transação</button>
+            <HeaderContent>
+                <img src={logoImg} alt="Logo da DT Money"></img>
+                <button type="button">Nova Transação</button>
+            </HeaderContent>
         </HeaderContainer>
     )
 }

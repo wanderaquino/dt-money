7ea0212a1b4882fe262b06db8c03bc368f1089b1
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 19:14:54 -0300

    Tracking transactions component.

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,7 +1,38 @@
 import { TransactionsContainer } from "./styles";
 
 export function Transactions () {
+    return (
     <TransactionsContainer>
-        <h1>Transactions</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Preço</th>
+                    <th>Categoria</th>
+                    <th>Data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Desenvolvimento de Site</td>
+                    <td>R$ 1000,00</td>
+                    <td>Venda</td>
+                    <td>10/04/2021</td>
+                </tr>
+                <tr>
+                    <td>Aluguel de Carro</td>
+                    <td>- R$ 200,00</td>
+                    <td>Transporte</td>
+                    <td>10/04/2021</td>
+                </tr>
+                <tr>
+                    <td>Mercado</td>
+                    <td>-R$ 600,00</td>
+                    <td>Alimentação</td>
+                    <td>10/04/2021</td>
+                </tr>
+            </tbody>
+        </table>
     </TransactionsContainer>
+    )
 }
--- src/components/Transactions/styles.ts
+++ src/components/Transactions/styles.ts
@@ -1,5 +1,34 @@
 import styled from "styled-components";
 
 export const TransactionsContainer = styled.div `
+    margin-top: 4rem;
+        table {
+            width: 100%;
+            border-spacing: 0 0.5rem;
+
+            th {
+                font-weight: 400;
+                padding: 1rem 2rem;
+                font-size: 1rem;
+                text-align: left;
+                color: var(--text-body);
+                line-height: 1.5rem;
+            }
+
+            td {
+                border-radius: 0.25rem;
+                background: var(--shape);
+                padding: 1rem 2rem;
+                border: 0;
+                color: var(--text-body);
+
+                &:first-child {
+                    color: var(--text-title);
+                }
+            }
+        }
+`
+
+export const TransactionItem = styled.tr `
 
 `

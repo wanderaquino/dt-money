0cee423c92afaeebd14b6d3e818f1148cbd5e09f
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 20:43:55 -0300

    Creating MirageJs server

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [{
+        id: 1,
+        title: "Transação 01",
+        amount: 400,
+        type: "deposit",
+        category: "Food",
+        createdAt:  new Date()
+      }]
+    })
+  }
+})
 
 ReactDOM.render(
     <App />,

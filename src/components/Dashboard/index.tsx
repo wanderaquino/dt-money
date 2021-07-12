365aee65c4486c23a11c7f4412d3c01bf0685679
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 19:14:14 -0300

    Rendering transactions component.

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { Transactions } from "../Transactions";
 import { DashboardContainer } from "./styles";
 
 export function Dashboard () {
     return (
         <DashboardContainer>
             <Summary />
+            <Transactions />
         </DashboardContainer>
     )
 }

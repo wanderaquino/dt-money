7dd0c6306cef021486a1de902ab814e706c60eb8
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 20:47:14 -0300

    Implementing useEffect hook for transactions route

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { TransactionsContainer } from "./styles";
 
 export function Transactions () {
+
+    useEffect(() => {
+        api.get("/transactions")
+            .then(response => console.log(response.data));
+    }, []);
+
     return (
     <TransactionsContainer>
         <table>

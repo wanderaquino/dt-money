138307b00f05a65401e4b7c5e386c8fc1c2a2453
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 17:56:18 -0300

    Adding Dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary";
+import { DashboardContainer } from "./styles";
+
+export function Dashboard () {
+    return (
+        <DashboardContainer>
+            <Summary />
+        </DashboardContainer>
+    )
+}

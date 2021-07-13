c9efe633fcabc7f2262751329cdce7c0bc32e3d0
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 21:34:33 -0300

    Rendering modal for new transaction on app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import {GlobalStyle} from "./styles/global";
 import { Header } from "./components/Header/index";
 import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { useState } from "react";
+Modal.setAppElement("#root");
+
 
 export function App() {
+  
+  const [isTransactionModalOpened, setIsTransactionModalOpened] = useState(false);
+
+  function handleOpenTransactionModal() {
+      setIsTransactionModalOpened(true);
+  }
+
+  function handleCloseTransactionModal() {
+      setIsTransactionModalOpened(false);
+  }
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTrasactionModal={handleOpenTransactionModal} />
       <Dashboard />
+      <Modal
+        isOpen={isTransactionModalOpened} 
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
     </>
   );
 }

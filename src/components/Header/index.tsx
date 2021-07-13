a42716adfcd7dba193c5c46ab5b03669fb7cf9cc
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 21:35:34 -0300

    Implementing interface for headerProps and call the function for the newTransactionMOdal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import logoImg from "../../assets/images/logo.svg";
 import { HeaderContainer, HeaderContent } from "./styles";
 
+interface HeaderProps {
+    onOpenNewTrasactionModal: () => void
+}
 
-export function Header() {
+export function Header({onOpenNewTrasactionModal}: HeaderProps) {
+
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="Logo da DT Money"></img>
-                <button type="button">Nova Transação</button>
+                <button type="button" onClick={onOpenNewTrasactionModal}>Nova Transação</button>
             </HeaderContent>
         </HeaderContainer>
     )

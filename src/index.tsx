c4dd11cc909ab56187b57e4f362f9f9e625abf98
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-14 01:06:35 -0300

    Adding post route for /transactions and populating the database for /get route

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
 import ReactDOM from 'react-dom';
 import {App} from './App';
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData(
+      {
+        transactions: [
+          {id: 1, name: "Transação de Trabalho", amount: 6000, type: "deposit", category: "Trabalho Extra", createdAt: new Date("2021-01-01 09:00:00")},
+          {id: 2, name: "Mercado", amount: 60, type: "withdraw", category: "Despesa Alimentos", createdAt: new Date("2021-01-02 09:00:00")},
+          {id: 3, name: "DayTrade", amount: 600, type: "deposit", category: "Trabalho Extra", createdAt: new Date("2021-01-03 09:00:00")},
+          {id: 4, name: "Reparo com Carro", amount: 900, type: "withdraw", category: "Despesa veículo", createdAt: new Date("2021-01-04 09:00:00")},
+          {id: 5, name: "Freelance Web", amount: 9000, type: "deposit", category: "Trabalho Extra", createdAt: new Date("2021-01-05 09:00:00")}
+        ]
+      }
+    )
+  },
   routes() {
     this.namespace = "api";
+
     this.get("/transactions", () => {
-      return [{
-        id: 1,
-        title: "Transação 01",
-        amount: 400,
-        type: "deposit",
-        category: "Food",
-        createdAt:  new Date()
-      }]
+      return this.schema.all("transaction");
+    })
+
+    this.post("/transactions", (schema, requestData) => {
+      const data = JSON.parse(requestData.requestBody);
+      return schema.create("transaction", data);
     })
   }
 })

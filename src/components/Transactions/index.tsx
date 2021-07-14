82e021cdd7bea0ca350a3c0d6ab2a2665d3e9b42
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-14 17:27:51 -0300

    Resolving styled container bug

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,9 +1,10 @@
 import { TransactionsContext } from "../../TransactionsContext";
-import { TransactionsContainer } from "./styles";
 import { useContext } from "react";
+import { TransactionsContainer } from "./styles";
 
 export function Transactions () {
-    const transactions = useContext(TransactionsContext);
+    const {transactions} = useContext(TransactionsContext);
+
     return (
     <TransactionsContainer>
         <table>
@@ -16,19 +17,22 @@ export function Transactions () {
                 </tr>
             </thead>
             <tbody>
-                {transactions.map((transaction) => (
+                {transactions.map(transaction => (
                 <tr key={transaction.id}>
                     <td>{transaction.name}</td>
-                    <td className={transaction.type}>{new Intl.NumberFormat(
+                    <td className={transaction.type}>
+                    {new Intl.NumberFormat(
                         "pt-BR", {
                             style: "currency",
                             currency: "BRL"
                         }
-                    ).format(transaction.amount)}</td>
+                    ).format(transaction.amount)}
+                    </td>
                     <td>{transaction.category}</td>
-                    <td>{transaction.createdAt}</td>
-                </tr>
-                ))}
+                    <td>
+                        {new Intl.DateTimeFormat("pt-BR", {dateStyle: "short"}).format(new Date(transaction.createdAt))}
+                    </td>
+                </tr>))}
             </tbody>
         </table>
     </TransactionsContainer>

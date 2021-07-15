7fe89499d06dc5b079b55665c955edc9ad89d68f
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-14 22:40:03 -0300

    Adding summary calc

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,8 +7,23 @@ import { Transactions } from "../Transactions";
 import { TransactionsContext } from "../../TransactionsContext";
 
 export function Summary () {
+    const {transactions} = useContext(TransactionsContext);
 
-    const transactions = useContext(TransactionsContext);
+    const totalTransactions = transactions.reduce((accumulator, transaction) => {
+            if (transaction.type === "deposit") {
+                accumulator.deposits += transaction.amount;
+                accumulator.total += transaction.amount
+            } else {
+                accumulator.withdraws += transaction.amount;
+                accumulator.total -= transaction.amount
+                }
+            return accumulator;
+            }, {
+              deposits: 0,
+              withdraws: 0,
+              total: 0
+        });
+
     return (
         <SummaryContainer>
             <div>
@@ -16,21 +31,31 @@ export function Summary () {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Imagem entrada financeira"></img>
                 </header>
-                <strong>R$ 1000,00</strong>
+                <strong>
+                    { 
+                        new Intl.NumberFormat("pt-BR", {style:"currency", currency: "BRL"})
+                            .format(totalTransactions.deposits)
+                    }
+                </strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Imagem saída financeira"></img>
                 </header>
-                <strong>- R$ 500,00</strong>
+                <strong>-{ 
+                        new Intl.NumberFormat("pt-BR", {style:"currency", currency: "BRL"})
+                        .format(totalTransactions.withdraws)
+                    }</strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total financeiro"></img>
                 </header>
-                <strong> R$ 500,00</strong>
+                <strong>{new Intl.NumberFormat("pt-BR", {style:"currency", currency: "BRL"})
+                            .format(totalTransactions.total)
+                    }</strong>
             </div>
         </SummaryContainer>
     )
